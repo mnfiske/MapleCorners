b9815db152fcdea4b5f7a42334099d6b2349af66
dlove14 <danielle.love145@gmail.com>
2021-01-25 17:29:37 -0700

    Add Energy and Money Basic Functionality

---
+++ MapleCorners/Assets/Scripts/Player/EnergyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyAttribute : MonoBehaviour
+{
+    public float playerEnergy;
+    //Serialize will make it visible in the Unity inspector, but it's still private to this script
+    [SerializeField] private Text energyText;
+
+    private void Start()
+    {
+        UpdateEnergy();
+    }
+    public void UpdateEnergy()
+    {
+        // this will update the UI text box that displays energy and set it to the player's energy (converted from float to string)
+        energyText.text = playerEnergy.ToString("0");
+    }
+}
---
+++ MapleCorners/Assets/Scripts/Player/ModifyEnergy.cs
@@ -0,0 +1,31 @@
+﻿// Code is referenced and adapted from a youtube tutorial on implementing a health system
+// 2D Health System for Unity - SpeedTutor (2019). Available at https://www.youtube.com/watch?v=tzEVJ3tKQUg
+
+using UnityEngine;
+
+public class ModifyEnergy : MonoBehaviour
+{
+    [SerializeField] private float bedEnergy;
+    [SerializeField] private EnergyAttribute energyController;
+
+    // if the player object collides with the bed object, it will reset the player's energy
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            ResetEnergy();
+        }
+    }
+
+    // this will reset energy to full stats (100)
+    void ResetEnergy()
+    {
+        // update energy variable
+        energyController.playerEnergy = 100;
+
+        // update UI to display new energy
+        energyController.UpdateEnergy();
+    }
+
+    //
+}
