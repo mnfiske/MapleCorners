761807c010b44b56fdaf59cfb252788f1ac58b4d
mnfiske <fiskem@oregonstate.edu>
2021-02-11 16:38:20 -0500

    Began work on Tilemap Grid

---
+++ MapleCorners/Assets/Scripts/Map/SO_GridProperties.cs
@@ -0,0 +1,18 @@
+﻿// "Citation: Unity 2D Game Developer Course Farming RPG"
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "so_GridProperties", menuName = "Scriptable Objects/Grid Properties")]
+public class SO_GridProperties : ScriptableObject
+{
+  public SceneName SceneName;
+  public int GridWidth;
+  public int GridHeight;
+  public int OriginX;
+  public int OriginY;
+
+  [SerializeField]
+  public List<GridProperty> GridPropertyList;
+}
