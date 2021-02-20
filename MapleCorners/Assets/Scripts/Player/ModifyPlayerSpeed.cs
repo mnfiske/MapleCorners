84050f1eb8690d3fa6b06899a0bf571ba5bc4b5e
Adamshrugged <spiveya@oregonstate.edu>
2021-02-20 17:53:12 -0600

    Mess & Rubbish Objects

---
+++ MapleCorners/Assets/Scripts/Player/ModifyPlayerSpeed.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ModifyPlayerSpeed : MonoBehaviour
+{
+    [SerializeField] private float newRunSpeed =  2.666f;
+    [SerializeField] private float newWalkSpeed = 1.333f;
+
+    // if the player object collides with the object, adjust the player's base speed
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Settings.DecMovementSpeed();
+        }
+    }
+
+    // after leaving collission, reset movement
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Settings.IncMovementSpeed();
+        }
+    }
+}
