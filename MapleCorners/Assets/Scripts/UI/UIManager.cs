7d8f4d9dd7dcc88a97c062605d1a67f3aa578a3d
mnfiske <fiskem@oregonstate.edu>
2021-02-26 22:39:34 -0500

    Pause menu inventory

---
+++ MapleCorners/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,140 @@
+﻿// "Citation: Unity 2D Game Developer Course Farming RPG"
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : SingletonMonoBehavior<UIManager>
+{
+    private bool _pauseMenuOn = false;
+    [SerializeField] private GameObject pauseMenu = null;
+    [SerializeField] private GameObject[] menuTabs = null;
+    [SerializeField] private Button[] menuButtons = null;
+    [SerializeField] private UIInventoryBar uiInventoryBar = null;
+    [SerializeField] private PauseMenuInventoryManagement pauseMenuInventoryManagement = null;
+
+    public bool PauseMenuOn { get => _pauseMenuOn; set => _pauseMenuOn = value; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        PauseMenu();
+    }
+
+    /// <summary>
+    /// Toggles the pause menu when the Escape key is pressed
+    /// </summary>
+    private void PauseMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseMenuOn)
+            {
+                DisablePauseMenu();
+            }
+            else
+            {
+                EnablePauseMenu();
+            }
+        }
+    }
+
+
+    private void EnablePauseMenu()
+    {
+        uiInventoryBar.DestroyCurrentlyDraggedItems();
+        uiInventoryBar.ClearCurrentlySelectedItems();
+
+        // Set the pause menu to on
+        PauseMenuOn = true;
+        // Disable player input
+        Player.Instance.PlayerInputIsDisabled = true;
+        // Pause the clock
+        Time.timeScale = 0;
+        // Set the pause menu canvas to active
+        pauseMenu.SetActive(true);
+
+        System.GC.Collect();
+
+        HighlightButtonForSelectedTab();
+    }
+
+    public void DisablePauseMenu()
+    {
+        // Destroy any items currently being dragged
+        pauseMenuInventoryManagement.DestroyCurrentlyDraggedItems();
+
+        // Set the pause menu to off
+        PauseMenuOn = false;
+        // Enable player input
+        Player.Instance.PlayerInputIsDisabled = false;
+        // Unpause the clock
+        Time.timeScale = 1;
+        // Turn off the pause menu
+        pauseMenu.SetActive(false);
+    }
+
+
+    private void HighlightButtonForSelectedTab()
+    {
+        for (int i = 0; i < menuTabs.Length; i++)
+        {
+            if (menuTabs[i].activeSelf)
+            {
+                SetButtonColorToActive(menuButtons[i]);
+            }
+
+            else
+            {
+                SetButtonColorToInactive(menuButtons[i]);
+            }
+        }
+    }
+
+
+    private void SetButtonColorToActive(Button button)
+    {
+        ColorBlock colors = button.colors;
+
+        colors.normalColor = colors.pressedColor;
+
+        button.colors = colors;
+
+    }
+
+    private void SetButtonColorToInactive(Button button)
+    {
+        ColorBlock colors = button.colors;
+
+        colors.normalColor = colors.disabledColor;
+
+        button.colors = colors;
+
+    }
+
+    public void SwitchPauseMenuTab(int tabNum)
+    {
+        for (int i = 0; i < menuTabs.Length; i++)
+        {
+            if (i != tabNum)
+            {
+                menuTabs[i].SetActive(false);
+            }
+            else
+            {
+                menuTabs[i].SetActive(true);
+
+            }
+        }
+
+        HighlightButtonForSelectedTab();
+
+    }
+}
---
+++ MapleCorners/Assets/Scripts/UI/UIPauseMenu/PauseMenuInventoryManagementSlot.cs
@@ -0,0 +1,105 @@
+﻿// "Citation: Unity 2D Game Developer Course Farming RPG"
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseMenuInventoryManagementSlot : MonoBehaviour
+{
+    public Image inventoryManagementSlotImage;
+    public TextMeshProUGUI textMeshProUGUI;
+    public GameObject greyedOutImageGO;
+    [SerializeField] private PauseMenuInventoryManagement inventoryManagement = null;
+    [SerializeField] private GameObject inventoryTextBoxPrefab = null;
+
+    [HideInInspector] public ItemDetails itemDetails;
+    [HideInInspector] public int itemQuantity;
+    [SerializeField] private int slotNumber = 0;
+
+    public GameObject draggedItem;
+    private Canvas parentCanvas;
+
+    private void Awake()
+    {
+        parentCanvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (itemQuantity != 0)
+
+        {
+            // Instantiate the item
+            draggedItem = Instantiate(inventoryManagement.inventoryManagementDraggedItemPrefab, inventoryManagement.transform);
+
+            // Get the image for the item & set the sprite
+            Image draggedItemImage = draggedItem.GetComponentInChildren<Image>();
+            draggedItemImage.sprite = inventoryManagementSlotImage.sprite;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (draggedItem != null)
+        {
+            // Set the position to the mouse position so the item will "follow" the mouse
+            draggedItem.transform.position = Input.mousePosition;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (draggedItem != null)
+        {
+            // Destory the dragged item
+            Destroy(draggedItem);
+
+            // Check if the player dragged the item onto another inventory slot
+            if (eventData.pointerCurrentRaycast.gameObject != null && eventData.pointerCurrentRaycast.gameObject.GetComponent<PauseMenuInventoryManagementSlot>() != null)
+            {
+                // If so, get the slot number of the slot the player dragged to
+                int toSlotNumber = eventData.pointerCurrentRaycast.gameObject.GetComponent<PauseMenuInventoryManagementSlot>().slotNumber;
+
+                // Swap the inventory items
+                InventoryManager.Instance.SwapInventoryItems(InventoryLocation.player, slotNumber, toSlotNumber);
+
+                // Destroy the inventory text box game object
+                inventoryManagement.DestroyInventoryTextBoxGameobject();
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (itemQuantity != 0)
+        {
+            inventoryManagement.inventoryTextBoxGameobject = Instantiate(inventoryTextBoxPrefab, transform.position, Quaternion.identity);
+            inventoryManagement.inventoryTextBoxGameobject.transform.SetParent(parentCanvas.transform, false);
+
+            UIInventoryTextbox inventoryTextBox = inventoryManagement.inventoryTextBoxGameobject.GetComponent<UIInventoryTextbox>();
+
+            string itemTypeDescription = InventoryManager.Instance.GetItemTypeDescription(itemDetails.itemType);
+
+            inventoryTextBox.SetTextboxText(itemDetails.itemDescription, itemTypeDescription, "", itemDetails.itemLongDescription, "", "");
+
+            if (slotNumber > 23)
+            {
+                inventoryManagement.inventoryTextBoxGameobject.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0f);
+                inventoryManagement.inventoryTextBoxGameobject.transform.position = new Vector3(transform.position.x, transform.position.y + 50f, transform.position.z);
+            }
+            else
+            {
+                inventoryManagement.inventoryTextBoxGameobject.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 1f);
+                inventoryManagement.inventoryTextBoxGameobject.transform.position = new Vector3(transform.position.x, transform.position.y - 50f, transform.position.z);
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        inventoryManagement.DestroyInventoryTextBoxGameobject();
+    }
+}
