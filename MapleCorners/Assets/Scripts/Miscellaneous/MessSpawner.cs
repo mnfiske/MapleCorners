5a7a0ae77023f822884d7d76717824baad87f893
Adamshrugged <spiveya@oregonstate.edu>
2021-02-28 11:16:53 -0600

    Spawn random mess objects daily

---
+++ MapleCorners/Assets/Scripts/Miscellaneous/MessSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MessSpawner : SingletonMonoBehavior<MessSpawner>
+{
+    MesObjectPooler objectPooler;
+    void Start()
+    {
+        objectPooler = MesObjectPooler.Instance;
+    }
+
+    // Register to time event system - once per day generate messes
+    private void OnEnable()
+    {
+        EventHandler.AdvanceGameDayEvent += dailyMesses;
+    }
+
+    private void OnDisable()
+    {
+        EventHandler.AdvanceGameDayEvent -= dailyMesses;
+    }
+
+    // Make the messes
+    private void dailyMesses()
+    {
+		objectPooler.SpawnFromPool("Mess1");
+	}
+
+
+
+    /*// Update once per frame
+	void FixedUpdate()
+	{
+		objectPooler.SpawnFromPool("Mess1");
+	}
+	*/
+}
